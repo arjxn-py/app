1061cba22820788a74f60453549dee6fec97edf2
arjxn.py <arjxn.py@gmail.com>
2022-10-08 14:08:12 +0530

    Custom CSS Added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+// Added bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import './custom.scss';
+// Add our style
+import './style/index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
